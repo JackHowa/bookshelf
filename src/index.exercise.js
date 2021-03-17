fca3c30659a9a2da1c76ffea16cfadf971419d88
jackhowa <5950956+JackHowa@users.noreply.github.com>
2021-03-17 17:46:55 -0500

    Simplify functionality on clicks

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -3,19 +3,15 @@ import ReactDOM from 'react-dom'
 import { Logo } from './components/logo'
 
 function App() {
-
-  function onButtonClick(message) {
-    alert(message)
-  }
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => onButtonClick('login')} type="button">Login</button>
+        <button onClick={() => alert('login')} type="button">Login</button>
       </div>
       <div>
-        <button onClick={() => onButtonClick('register')} type="button">Register</button>
+        <button onClick={() => alert('register')} type="button">Register</button>
       </div>
     </div>
   )
