3694feb64074ddb7778fe2e32fca7a3493fc5b51
jackhowa <5950956+JackHowa@users.noreply.github.com>
2021-03-17 17:59:40 -0500

    Use reach dialog box with aria label per tests

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -2,17 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import { Logo } from './components/logo'
 
+// show reach dialog boxes
+import { Dialog } from "@reach/dialog";
+import '@reach/dialog/styles.css';
+
 function App() {
+  const [isLoginOpen, setIsLoginOpen] = React.useState(false)
+  const [isRegistrationOpen, setIsRegistrationOpen] = React.useState(false)
+
+  function toggleIsLoginOpen() {
+    setIsLoginOpen(prevState => !prevState)
+  }
+
+  function toggleIsRegistrationOpen() {
+    setIsRegistrationOpen(prevState => !prevState)
+  }
+
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('login')} type="button">Login</button>
+        <button onClick={toggleIsLoginOpen} type="button">Login</button>
       </div>
       <div>
-        <button onClick={() => alert('register')} type="button">Register</button>
+        <button onClick={toggleIsRegistrationOpen} type="button">Register</button>
       </div>
+      <Dialog aria-label={"register"} isOpen={isRegistrationOpen} onDismiss={toggleIsLoginOpen}>
+        <p>Register</p>
+      </Dialog>
+      <Dialog aria-label={"login"} isOpen={isLoginOpen} onDismiss={toggleIsRegistrationOpen}>
+        <p>Login</p>
+      </Dialog>
     </div>
   )
 }
